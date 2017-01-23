8c97caa66c0aa4b68d5320219c39003121970737
mihaiolteanu <mihai_olteanu@fastmail.fm>
2017-01-23 10:37:50 +0200

    Add shell script variant

---
+++ lyrics.sh
@@ -0,0 +1,116 @@
+#!/usr/bin/env zsh
+
+# Get lyrics for the currently playing song in cmus.
+# To display the lyrics in stumpwm, for example, define a new command
+# (defcommand player-lyrics () ()
+#     (message-no-timeout (run-shell-command "/path/to/lyrics.sh" t)))
+
+db="$HOME/lyrics"
+
+get-cmus-tag() {
+    local cmus_tag=$1
+    cmus-remote -Q | grep $cmus_tag | sed "s/$cmus_tag //"
+}
+
+get-artist() {
+    local artist=$(get-cmus-tag "tag artist")
+    echo $artist
+}
+
+get-title() {
+    local title=$(get-cmus-tag "tag title")
+    echo $title
+}
+
+# URL queries can't contain spaces.
+prepare-for-query() {
+    echo $1 | sed "s/ /+/g"
+}
+
+# Location of the lyrics in the db for this artist and song.
+get-lyrics-location() {
+    local artist=$1
+    local title=$2
+    local location=$db"/"$artist"/"$title
+    echo $location
+}
+
+# Location of the artist in the db.
+get-artist-location() {
+    local artist=$1
+    local location=$db"/"$artist
+    echo $location
+}
+
+# Fetch the lyrics from the database, if they exist.
+get-db-lyrics() {
+    local artist=$1
+    local title=$2
+    local lyrics=""
+    local location=$(get-lyrics-location $artist $title)
+    if [[ -a $location ]]; then
+        lyrics=$(<$location)
+    fi
+    echo $lyrics
+}
+
+# Update the db with these lyrics, if needed.
+save-lyrics-db() {
+    local artist=$1
+    local title=$2
+    local lyrics=$3
+    local location=$(get-lyrics-location $artist $title)
+    if [[ ! -a $location ]]; then
+        mkdir -p $(get-artist-location $artist)
+        echo $lyrics > $location
+    fi        
+}
+
+# Get lyrics from makeitpersonal.
+get-mp-lyrics() {
+    local artist=$1
+    local title=$2
+    local source="https://makeitpersonal.co/lyrics?artist=%s&title=%s"
+    local request=$(printf $source $artist $title)
+    local lyrics=$(curl -s $request)
+    if [[ $lyrics =~ "Sorry, We don't have lyrics for this song yet" ]]; then
+        lyrics=""              # Bad luck this time.
+    fi
+    echo $lyrics
+}
+
+# Get lyrics from darklyrics.
+get-dl-lyrics() {
+    local artist=$1
+    local title=$2
+    echo ""
+}
+
+get-lyrics() {
+    local artist=$1
+    local title=$2
+    # Try multiple sources, pick the first that returns a result or
+    # fail in agony. (command subtitution removes newline chars,
+    # quoting the whole thing prevents it)
+    local lyrics="${$(get-db-lyrics $artist $title):- \
+                  ${$(get-mp-lyrics $artist $title):- \
+                  ${$(get-dl-lyrics $artist $title):-
+                  ""}}}"
+    if [[ ! -z "${lyrics// }" ]]; then
+        save-lyrics-db $artist $title $lyrics
+    fi
+    echo $lyrics
+}
+
+main() {
+    local raw_artist=$(get-artist)
+    local raw_title=$(get-title)
+    # Raw values only used for printing, for all other activities I
+    # need a nice format without spaces.
+    local artist=$(prepare-for-query $raw_artist)
+    local title=$(prepare-for-query $raw_title)
+    local lyrics=$(get-lyrics $artist $title)
+    printf "%s - %s \n %s" $raw_artist $raw_title $lyrics
+}
+
+main
