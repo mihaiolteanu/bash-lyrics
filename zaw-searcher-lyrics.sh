63d5b58a140f93394980137d6caef2db0597c902
mihaiolteanu <mihai_olteanu@fastmail.fm>
2017-02-04 11:44:35 +0200

    Add an interactive lyrics searcher script using zaw

---
+++ zaw-searcher-lyrics.sh
@@ -0,0 +1,38 @@
+# zaw source for ack/ag searcher
+
+autoload -U read-from-minibuffer
+
+function zaw-lyrics-src-searcher() {
+    local buf
+    read-from-minibuffer "lyrics: "
+    if [[ -z "${REPLY// }" ]]; then
+        buf=$(find ~/lyrics -type f)
+    else
+        buf=$(ag -i $REPLY ~/lyrics/)
+    fi
+    if [[ $? != 0 ]]; then
+        return 1
+    fi
+    : ${(A)candidates::=${(f)buf}}
+    : ${(A)cand_descriptions::=${(f)buf}}
+    actions=(\
+             zaw-lyrics-src-searcher-play \
+        )
+    act_descriptions=(\
+                      "Cat" \
+        )
+}
+
+function zaw-lyrics-src-searcher-play () {
+    local filename=${1%%:*}
+    local array=(${(s:/:)filename})
+    local title=$array[-1]
+    local artist=$array[-2]
+    local command=$(printf "/%s %s" ${artist//-/ } ${title//-/ })
+    echo $artist "-" $title
+    cmus-remote -C $command
+    cmus-remote -C "win-activate"
+    zle accept-line
+}
+
+zaw-register-src -n lyrics-searcher zaw-lyrics-src-searcher
