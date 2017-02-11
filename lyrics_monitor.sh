60cc97be7573159f1ff9f3267d0b6934eac726d3
mihaiolteanu <mihai_olteanu@fastmail.fm>
2017-02-11 10:25:09 +0200

    Search in folder if the monitored file is a folder

--- lyrics_monitor.sh
+++ lyrics_monitor.sh
@@ -3,5 +3,11 @@
 MONITORDIR="$HOME/music/"
 inotifywait -m -r -e create -e move --format '%w%f' "${MONITORDIR}" | while read NEWFILE
 do
-    ./lyrics.sh -e "false" -f $NEWFILE
+    if [[ -d $NEWFILE ]]; then
+        for file in $NEWFILE/**/*.mp3; do
+            ~/projects/lyrics/lyrics.sh -e "false" -f $file
+        done
+    else
+        ~/projects/lyrics/lyrics.sh -e "false" -f $NEWFILE
+    fi
 done
